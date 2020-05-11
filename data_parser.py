5147d42c2b25ceafaccbac5362189547de191da2
morwat9 <morwat9@hotmail.com>
2020-05-11 16:23:20 -0700

    Altered dialog and input prompts. Added functionality to add new players, remove players from current poker session, and update scores whether or not players are in the current session or not.

--- data_parser.py
+++ data_parser.py
@@ -3,7 +3,9 @@
 
 def extract_players(player_file):
     """Extracts players and scores, then returns converted data"""
+
     player_data = {}
+
     with open(player_file) as file_object:
         for line in file_object:
             # Captures player name
@@ -20,7 +22,15 @@ def extract_players(player_file):
 
 def extract_dates(date_file):
     """Extracts dates and returns a list of them"""
+
     return open(date_file).readline().split(',')
+
+
+def add_date(date):
+    """Adds the date of the current session to the date file"""
+
+    with open('dates.txt', 'a') as file_object:
+        file_object.write("," + date)
 
 
 def add_player_results(new_results):
@@ -32,12 +42,50 @@ def add_player_results(new_results):
 
     # Make a copy of contents, update copy and rewrite entire file
     lines_new = []
-    for k, v in new_results.items():
-        for line in lines:
+
+    for line in lines:
+        # If player played, update their score with incoming data
+        for k, v in new_results.items():
             if k in line:
                 lines_new.append(line.strip() + f',{v}' + '\n')
+
+        # If player didn't play, last score is repeated
+        if line.split(':').pop(0) not in new_results:
+            last_score = line.split(',').pop()
+            lines_new.append(line.strip() + f',{last_score}')
+
+    lines_new.sort()
 
     with open('player_data.txt', 'w') as file_object:
         file_object.writelines(lines_new)
 
-    import visual
+
+def add_new_player(new_player):
+    """Add a new player to database and start during this date"""
+
+    # Open file and get contents
+    with open('player_data.txt') as file_object:
+        lines = file_object.readlines()
+
+    # Get the number of games played
+    game_count = len(lines.copy().pop().split(','))
+
+    # Create new copy of data
+    lines_new = []
+    for line in lines:
+        lines_new.append(line)
+
+    # Create formatted line for new player in database
+    new_player_line = [new_player + ":"]
+
+    for i in range(game_count):
+        new_player_line.append("0,")
+
+    str_line = "".join(new_player_line)
+    str_line = str_line[:-1] + '\n'
+
+    lines_new.append(str_line)
+    lines_new.sort()
+
+    with open('player_data.txt', 'w') as file_object:
+        file_object.writelines(lines_new)
--- input_prompts.py
+++ input_prompts.py
@@ -1,58 +1,159 @@
 """Input tree for gathering poker data"""
 
-from data_parser import extract_players, add_player_results
+import sys
+from data_parser import extract_players, add_player_results, add_new_player, add_date
 
 
 def startup_prompt():
     """Find out what user wants to do upon opening program"""
+
     option = input('Type "start" to enter new poker data, "view" to see the current chart, or "quit" to quit.\n')
 
     if option == 'quit':
-        quit
+        sys.exit()
     elif option == 'view':
         import visual
         startup_prompt()
     elif option == 'start':
-        get_session_info()
+        new_players_prompt()
     else:
         startup_prompt()
 
 
-def get_session_info():
-    """Gather info about who's playing and the date"""
-    date = input("Enter today's date (dd/mm):\n")
-    confirm = input(f"Today is {date}. Is this correct? (y/n):\n")
-    if confirm != 'y':
-        get_session_info()
+def date_prompt():
+    """Get the date for the current poker session"""
+
+    date = input("Enter today's date (m/d): ")
+
+    date_confirm = input(f"Today is {date}. Is this correct? (y/n): ")
+
+    if date_confirm != 'y':
+        date_prompt()
     else:
-        with open('dates.txt', 'a') as file_object:
-            file_object.write("," + date)
+        # Add date to database, then ask about new players
+        add_date(date)
         print("Date saved.")
-        # TODO: Show roster and ask more about who's playing
-        # print("\nHere's the current poker roster.")
-        # for player in players:
-        #     print(player.title())
-        # TODO: Are any players missing? Omit from adding new scores or keep their score
-        # TODO: Any new players? Add them to player_data.txt
-        # TODO: Maybe instead of end result, ask how much they gained or lost then do math to get end result
-        # confirm_winnings(players)
-        confirm_players()
+        confirm_players_prompt()
 
 
-def confirm_winnings(players):
-    score_update_dict = {}
+def new_players_prompt():
+    """Inquire about possible new players and redirect accordingly"""
+
+    new_player_confirm = input("Are there new players today? (y/n): ")
+
+    if new_player_confirm == 'y':
+        add_new_player_prompt()
+    elif new_player_confirm == 'n':
+        date_prompt()
+    else:
+        new_players_prompt()
+
+
+def confirm_winnings_prompt(players):
+    """Takes in standings after the game and adds to the database"""
+
+    # Asks for each players new standing and stores in score_update
+    score_update = {}
+
     for player in players:
         result = input(f"End result for {player.title()}?: ")
-        score_update_dict[player] = result
+        score_update[player] = result
 
+    # Confirms standings saves to database
     save_confirm = input('Please review final results. Save and view graph? (y/n)')
     if save_confirm == 'y':
-        add_player_results(score_update_dict)
+        add_player_results(score_update)
+        import visual
+        sys.exit()
     else:
-        confirm_winnings(players)
+        confirm_winnings_prompt(players)
 
-def confirm_players():
+
+def confirm_players_prompt():
+    """Asks if all registered players are playing."""
+
+    # Shows all registered players
     players = [player for player, scores in extract_players('player_data.txt').items()]
+
     print("\nHere's the current poker roster.")
+
     for player in players:
         print(player.title())
+
+    # If there are missing players, take them out of this session
+    all_players_confirm = input("Is everyone playing today? (y/n): ")
+
+    if all_players_confirm == 'y':
+        confirm_winnings_prompt(players)
+    elif all_players_confirm == 'n':
+        remove_missing_players_prompt(players)
+
+
+def remove_missing_players_prompt(players):
+    """Removes players who aren't playing today and retains score for missing players"""
+
+    # Asks for missing players, one at a time
+    missing_players = []
+
+    print("Who isn't playing? Type in each player's name as it was listed one at a time")
+    print("Type 'done' when you're finished.\n")
+
+    player = ""
+
+    while player != 'done':
+        player = input("Player: ")
+
+        if player.lower() in players:
+            missing_players.append(player.lower())
+        elif player not in players:
+            print("Can't find that player. Re-enter with no spelling mistakes.")
+
+    # If they 'done' was entered without any players, returns to confirm_players_prompt()
+    if not missing_players:
+        confirm_players_prompt()
+    else:
+        # Confirms missing players and retains the current players
+        print("These are the missing players.\n")
+        for player in missing_players:
+            print(player.title())
+
+        missing_player_confirm = input("Is this correct? (y/n): ")
+
+        if missing_player_confirm != 'y':
+            remove_missing_players_prompt(players)
+        else:
+            # Sends a list of the current players to the confirm winnings prompt
+            current_players = []
+            for player in players:
+                if player not in missing_players:
+                    current_players.append(player)
+            confirm_winnings_prompt(current_players)
+
+
+def add_new_player_prompt():
+    """Asks about new player and adds them to the database"""
+
+    print("What is the new player's name? Must be one word, and must be unique.")
+    print("Type 'cancel' to proceed without adding a new player.")
+
+    new_player = input("Player: ")
+
+    if new_player == 'cancel':
+        confirm_players_prompt()
+    else:
+        confirm_player = input(f"New player's name is {new_player}. Is this correct? (y/n): ")
+
+        if confirm_player != 'y':
+            add_new_player_prompt()
+        else:
+            add_new_player(new_player.lower())
+
+            print("Player saved.")
+
+            # Ask if there's any more players to add to the roster
+            add_another_player_confirm = input("Is there another player you'd like to add? (y/n): ")
+
+            if add_another_player_confirm != 'y':
+                date_prompt()
+            else:
+                add_new_player_prompt()
